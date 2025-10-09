7fa21c9de2d9f988b57634202ef6741782a744c1
Johannes <johje@itu.dk>
2025-10-09 13:29:46 +0200

    Server can now start without error and proto files can be generated...

--- server/server.go
+++ server/server.go
@@ -1,2 +1,35 @@
-package server;
+package main
 
+import (
+	"log"
+	"net"
+	proto "handin3/grpc"
+	"google.golang.org/grpc" 
+)
+
+type ChitChatServer struct {
+	proto.UnimplementedChitChatServer
+}
+
+func main() {
+	server := &ChitChatServer{}
+
+	server.start_server()
+}
+
+func (s *ChitChatServer) start_server() {
+	grpcServer := grpc.NewServer()
+	listener, err := net.Listen("tcp", ":5050")
+	if err != nil {
+		log.Fatalf("Did not work")
+	}
+
+	proto.RegisterChitChatServer(grpcServer, s)
+
+	err = grpcServer.Serve(listener)
+
+	if err != nil {
+		log.Fatalf("Did not work")
+	}
+
+}
